31e1b1790c38e25f9a303624b4c0b67c1ccfcd4b
ibirossi <ibi.homaidan@gmail.com>
2021-05-10 17:07:00 +0200

    set up links in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,44 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { Menu } from "antd";
+import { HomeOutlined, UserOutlined, UserAddOutlined } from "@ant-design/icons";
 
-
+const { SubMenu, Item } = Menu;
 
 
 const Header = () => {
   const [current, setCurrent] = useState("home");
-  
 
   const handleClick = (e) => {
     setCurrent(e.key);
   };
   return (
     <Menu
-      
       onClick={handleClick}
       selectedKeys={[current]}
       mode="horizontal"
       className="custom-class"
     >
-      <Menu.Item key="home" className="custom-class">
-        Home
-      </Menu.Item>
-      <Menu.Item key="signup" className="custom-class">
-        Signup
-      </Menu.Item>
-      <Menu.Item key="login" className="custom-class">
-        Log In
-      </Menu.Item>
+      <Item key="home" icon={<HomeOutlined />}>
+        <Link exact to="/">Home</Link>
+      </Item>
 
-      <Menu.Item key="setting:1" className="custom-class">
-        Shop
-      </Menu.Item>
-      <Menu.Item key="setting:2" className="custom-class">
-        Checkout
-      </Menu.Item>
+      <Item
+        key="signup"
+        icon={<UserAddOutlined />}
+        className="float-right"
+      >
+        <Link to="/signup">Sign Up</Link>
+      </Item>
+
+      <Item key="login" icon={<UserOutlined />} className="float-right">
+        <Link to="/login">Log In</Link>
+      </Item>
+
+      <SubMenu title="Username">
+        <Item key="setting:1">Shop</Item>
+        <Item key="setting:2">Checkout</Item>
+      </SubMenu>
     </Menu>
   );
 };
